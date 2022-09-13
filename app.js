189acc1f1d190b910d5b0c662b429faf0d385552
petermc87 <peter.mcgibney@gmail.com>
2022-09-12 21:24:58 -0400

    input keys for moving the player fish. animate one fish movement across the screen

--- app.js
+++ app.js
@@ -2,12 +2,18 @@
 // -------- GLOBAL VARIABLES -------//
 // ---------------------------------//
 
+let move = 15;
+
 // ---------------------------------//
 // -----------CACHED DOMS-----------//
 // ---------------------------------//
 
 const allEatingFish = document.querySelector('.all-fish')
 const playerSprite = document.querySelector('.player-sprite')
+const krillSprite = document.getElementById('krill')
+const redSnapper = document.getElementById('red-snapper')
+const goldFish = document.getElementById('gold-fish')
+const playerMouth = document.querySelector('.mouth-contact')
 
 // ---------------------------------//
 // -------------CLASSES-------------//
@@ -21,65 +27,153 @@ const playerSprite = document.querySelector('.player-sprite')
 // ------------FUCNCTIONS ----------//
 // ---------------------------------//
 
+const collisionCheck = (food, player) => {
+    let foodRect = food.getBoundingClientRect();
+    let playerRect = player.getBoundingClientRect(); 
+
+    // return a reaction between 2 divs from all directions
+    return (foodRect.right >= playerRect.left && foodRect.left <= playerRect.right) && 
+    (foodRect.bottom >= playerRect.top && foodRect.top <= playerRect.bottom);
+  }
+
 
 // ---------------------------------//
 // -------- EVENT LISTENERS --------//
 // ---------------------------------//
 
 //Key up, down, left and right for moving the sprite
+window.addEventListener('load', () => {
+    playerSprite.style = `position: abolsute;
+    left: 0;
+    top: 100px;`
+});
 
 
-window.addEventListener('keydown', checkKeyPress, false);
-
-
-function checkKeyPress(key){
-    if (key.keyCode == '65'){
-        // alert('the left arrow was pressed')
-        playerSprite.style = 'transform: rotate(180deg);'
-        playerSprite.style = 'transform: rotateY(180deg);'
+window.addEventListener('keydown', (evt) => {
+    switch(evt.key){
+        case 'ArrowLeft':
+           
+            // playerSprite.style = 'transform: rotate(180deg);'
+            // playerSprite.style = 'transform: rotateY(180deg);'
+            playerSprite.style.left = parseInt(playerSprite.style.left) - move + 'px';
+            
+            break;
+        case 'ArrowRight':
+                // console.log('it works')
+            playerSprite.style.left = parseInt(playerSprite.style.left) + move + 'px';
+            break;
+        case 'ArrowUp':
+                // console.log('it works')
+            // playerSprite.style = 'transform: rotate(-90deg);'
+            playerSprite.style.top = parseInt(playerSprite.style.top) - move + 'px';
+            break;
+        case 'ArrowDown':
+                // console.log('it works')
+            playerSprite.style.top = parseInt(playerSprite.style.top) + move + 'px';
+            break;
     }
 
-    else if (key.keyCode == '68'){
-        //right using d
-        playerSprite.style = `transform: rotate(0deg);`
-        // playerSprite.style = 'animation: sprite-animation 1s infinite;'
-    }
+    setTimeout(function() {
 
-    else if (key.keyCode == '87'){
-        // move up with w
-        playerSprite.style = 'transform: rotate(-90deg);'
-        // playerSprite.style = 'animation: sprite-animation 1s infinite;'
-
-    }
+        if (collisionCheck(krillSprite, playerMouth)) {
+                krillSprite.style = `display: none;`
+            }
+        else if (collisionCheck(redSnapper, playerMouth)) {
+                redSnapper.style = `display: none;`
+            }
+        else if (collisionCheck(goldFish, playerMouth)) {
+                goldFish.style = `display: none;`
+            }
+      }, 150);
     
-    else if (key.keyCode == '83'){
-        // down using s
-        playerSprite.style = 'transform: rotate(90deg);'
+});
 
-    }   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let modifier = 5;
+
+// window.addEventListener('keydown', (evt) => {
+//     const {style} = playerSprite;
+//     switch (evt.key){
+//         case 'ArrowUp': style.top = `${parseInt(style.top) - modifier}`; break;
+//         case 'ArrowDown': style.top = `${parseInt(style.top) + modifier}`; break;
+//     }
+// })
+
+
+
+// window.addEventListener('keydown', checkKeyPress, false);
+
+
+// function checkKeyPress(key){
+//     if (key.keyCode == '65'){
+//         // alert('the left arrow was pressed')
+//         playerSprite.style = 'transform: rotate(180deg);'
+//         playerSprite.style = 'transform: rotateY(180deg);'
+//     }
+
+//     else if (key.keyCode == '68'){
+//         //right using d
+//         playerSprite.style = `transform: rotate(0deg);`
+//         // playerSprite.style = 'animation: sprite-animation 1s infinite;'
+//     }
+
+//     else if (key.keyCode == '87'){
+//         // move up with w
+//         playerSprite.style = 'transform: rotate(-90deg);'
+//         // playerSprite.style = 'animation: sprite-animation 1s infinite;'
+
+//     }
+    
+//     else if (key.keyCode == '83'){
+//         // down using s
+//         playerSprite.style = 'transform: rotate(90deg);'
+
+//     }   
     
 
-    else if (key.keyCode == '69'){
-        //up and right using e
-        playerSprite.style = 'transform: rotate(-30deg);'
+//     else if (key.keyCode == '69'){
+//         //up and right using e
+//         playerSprite.style = 'transform: rotate(-30deg);'
       
-    }   
+//     }   
 
-    else if (key.keyCode == '88'){
-        //down and right with x
-        playerSprite.style = 'transform: rotate(30deg);'
+//     else if (key.keyCode == '88'){
+//         //down and right with x
+//         playerSprite.style = 'transform: rotate(30deg);'
 
-    }   
-    else if (key.keyCode == '81'){
-        //down and right with x
-        // playerSprite.style = 'transform: rotateY(180deg);'
-        playerSprite.style = 'transform: rotate(-150deg);'
-        // playerSprite.style = 'transform: rotateY(180deg);'
+//     }   
+//     else if (key.keyCode == '81'){
+//         //down and right with x
+        
+//         playerSprite.style = 'transform: rotate(-150deg);'
+//         playerSprite.style = 'transform: rotateY(-180deg);'
 
-    }   
+//     }   
 
 
-}
+// }
 
 
 
